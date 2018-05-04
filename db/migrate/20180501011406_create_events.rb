0ec4644609c832ab2501f3bf8c2a8cf42dc81f7d
Jay Davila <jaydavila94@icloud.com>
2018-05-03 23:35:49 -0400

    update

--- db/migrate/20180501011406_create_events.rb
+++ db/migrate/20180501011406_create_events.rb
@@ -7,13 +7,8 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.string :date
       t.string :time
 
-<<<<<<< HEAD
-      t.references :admin
-      t.references :dog
-=======
       t.references :dog
       t.references :admin
->>>>>>> d46bdec72ecc645c25a668c65e832de27a0cbde0
 
       t.timestamps
     end
