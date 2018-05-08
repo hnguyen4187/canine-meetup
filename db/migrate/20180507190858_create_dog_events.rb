a97e5131c767d4a10ef2b2f365df5279e04dfcb1
Kobi Anderson <Kanderson10222@gmail.com>
2018-05-07 21:58:39 -0400

    styled application

--- db/migrate/20180507190858_create_dog_events.rb
+++ db/migrate/20180507190858_create_dog_events.rb
@@ -3,6 +3,7 @@ class CreateDogEvents < ActiveRecord::Migration[5.2]
     create_table :dog_events do |t|
       t.integer :dog_id
       t.integer :event_id
+      
       t.timestamps
     end
   end
